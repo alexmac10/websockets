ecb34ca20ce80634c7281bdb0c2db3994c8aaeed
theremsoe <theremsoe@gmail.com>
2015-08-13 14:30:34 -0500

    Ahora nuestro servidor implementa el proxocolo WebSockets

--- composer/MyApp/chat-server.php
+++ composer/MyApp/chat-server.php
@@ -1,11 +1,17 @@
 <?php
 	use Ratchet\Server\IoServer;
+	use Ratchet\Http\HttpServer;
+	use Ratchet\WebSocket\WsServer;
 
 	require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 	require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'MyApp' . DIRECTORY_SEPARATOR . 'Chat.php';
 
 	$server = IoServer::factory(
-		new MyApp\Chat(),
+		new HttpServer(
+            new WsServer(
+                new MyApp\Chat()
+            )
+        ),
 		8080
 	);
 
